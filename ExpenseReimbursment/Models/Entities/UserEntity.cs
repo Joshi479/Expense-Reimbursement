073cbb7b9ea5b53d3a62a2f55371a08698a0f3e7
Joshi479 <joshisaiteja79@gmail.com>
2017-02-26 18:14:26 -0800

    Addeed login logic

--- ExpenseReimbursment/Models/Entities/UserEntity.cs
+++ ExpenseReimbursment/Models/Entities/UserEntity.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using ExpenseReimbursment.Models.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExpenseReimbursment.Models.Entities
 {
-    public class User
+    public class UserEntity
     {
         public int UserId { get; set; }
         public string Password { get; set; }
+ 
     }
 }
